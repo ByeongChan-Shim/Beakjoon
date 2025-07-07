72c38a849e01fa49c7784f97311f712325b1c30b
ByeongChan-Shim <asx13579@naver.com>
2025-07-07 22:59:14 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int a;
+    cin >> a;
+    for (int i = 1; i <= a; ++i) {
+        cout << i << "\n";
+    }
+}
