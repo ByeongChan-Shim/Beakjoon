e8d55b6b616f92a04da83cbdd3cfde3cccd126d0
ByeongChan-Shim <asx13579@naver.com>
2025-08-17 22:28:45 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    while (n--) {
+        string s;
+        cin >> s;
+        cout << s << "DORO" << " ";
+    }
+}
