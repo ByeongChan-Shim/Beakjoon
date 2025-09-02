98cf2b587246a4869066128cc02e3211f484502f
ByeongChan-Shim <asx13579@naver.com>
2025-09-02 11:53:53 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int tt;
+    cin >> tt;
+    vector<long long> v(101);
+    v[1] = 1;
+    v[2] = 1;
+    v[3] = 1;
+    for (int i = 4; i <= 100; i++) v[i] = v[i - 2] + v[i - 3];
+    while (tt--) {
+        int n;
+        cin >> n;
+        cout << v[n] << "\n";
+    }
+}
